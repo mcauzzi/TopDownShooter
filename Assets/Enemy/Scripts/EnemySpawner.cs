468647f038b4c009f2e9e73a78f3bbf1fb5459b0
furai93@gmail.com <>
2025-05-19 21:17:40 +0200

    Added looping waves

--- Assets/Enemy/Scripts/EnemySpawner.cs
+++ Assets/Enemy/Scripts/EnemySpawner.cs
@@ -9,6 +9,8 @@ namespace Enemy.Scripts
     {
         [SerializeField] private List<WaveConfigSO> waveConfigs;
         [SerializeField] private float              timeBetweenWaves = 0.5f;
+        [SerializeField] private bool               isLooping        = false;
+
         void Start()
         {
             StartCoroutine(SpawnEnemies());
@@ -16,12 +18,14 @@ namespace Enemy.Scripts
 
         private IEnumerator SpawnEnemies()
         {
-            foreach (var waveConfig in waveConfigs)
+            do
             {
-                yield return StartCoroutine(SpawnAllEnemiesInWave(waveConfig));
-                yield return new WaitForSeconds(timeBetweenWaves);
-            }
-         
+                foreach (var waveConfig in waveConfigs)
+                {
+                    yield return StartCoroutine(SpawnAllEnemiesInWave(waveConfig));
+                    yield return new WaitForSeconds(timeBetweenWaves);
+                }
+            } while (isLooping);
         }
 
         private IEnumerator SpawnAllEnemiesInWave(WaveConfigSO waveConfig)
@@ -30,7 +34,7 @@ namespace Enemy.Scripts
             {
                 var enemyPrefab = waveConfig.GetEnemyPrefab(i);
                 var spawnPoint  = waveConfig.GetStartingPoint();
-                var enemy       = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity,transform);
+                var enemy       = Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, transform);
                 var pathfinder  = enemy.GetComponent<Pathfinder>();
                 pathfinder.WaveConfig = waveConfig;
                 yield return new WaitForSeconds(waveConfig.GetEnemyInterval());
